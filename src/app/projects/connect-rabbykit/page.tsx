f9171058c6982deea1b757971fcee8c879b1d606
Francis Berger <fberger@coinshares.com>
2024-11-04 22:46:09 +0100

    feat: setup

--- src/app/projects/connect-rabbykit/page.tsx
+++ src/app/projects/connect-rabbykit/page.tsx
@@ -32,7 +32,8 @@ export default function Page() {
             </div>
             <div className="flex w-full flex-col gap-2 border-t border-light-hover py-4">
                 <p className="text-secondary">2. Why</p>
-                <p className="text-sm text-default">- Based on @wagmi/core and viem + great Desktop/Mobile/Extension integration</p>
+                <p className="text-sm text-default">- based on @wagmi/core and viem + great Desktop/Mobile/Extension integration</p>
+                <p className="text-sm text-default">- great Desktop/Mobile/Extension integration</p>
             </div>
             <div className="flex w-full flex-col gap-1 border-t border-light-hover py-4">
                 <p className="text-secondary">Conclusion</p>
