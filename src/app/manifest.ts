93dffa4eca5e5a9a78989fd915f6a98569a9b14e
Francis Berger <fberger@coinshares.com>
2024-11-07 17:31:34 +0100

    feat: setup

--- src/app/manifest.ts
+++ src/app/manifest.ts
@@ -4,7 +4,6 @@ import { MetadataRoute } from 'next'
 export default function manifest(): MetadataRoute.Manifest {
     return {
         name: APP_METADATA.SITE_NAME,
-        short_name: APP_METADATA.SITE_NAME,
         description: APP_METADATA.SITE_DESCRIPTION,
         lang: 'en',
         start_url: '/',
