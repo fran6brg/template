1dc172aaf0d3c811cd18c0d696f7e8f97f529253
Francis Berger <frcs.berger@gmail.com>
2024-11-20 00:03:29 +0100

    feat: setup

--- src/components/layouts/Header.tsx
+++ src/components/layouts/Header.tsx
@@ -9,7 +9,7 @@ import { APP_METADATA } from '@/config/app.config'
 export default function Header(props: { className?: string }) {
     return (
         <div className={cn('fixed top-0 flex justify-center items-center w-full', props.className)}>
-            <div className="relative flex h-20 w-full max-w-[600px] items-center justify-between gap-0.5 border-b border-very-light-hover bg-background px-4 text-base backdrop-blur-md sm:mt-2 sm:h-fit sm:justify-between sm:rounded-lg sm:border-transparent sm:bg-transparent sm:p-2 sm:text-lg">
+            <div className="relative flex h-[70px] w-full max-w-[600px] items-center justify-between gap-0.5 border-b border-very-light-hover bg-background px-4 text-base backdrop-blur-md sm:mt-2 sm:h-fit sm:justify-between sm:rounded-lg sm:border-transparent sm:bg-transparent sm:p-2 sm:text-lg">
                 <div className="bg-background/10 absolute inset-0 z-40 rounded-xl" />
                 <LinkWrapper href="/" className="z-50 flex items-end gap-2 md:hidden">
                     <Image src={APP_METADATA.PROFILE_PICTURE} width={36} height={36} alt="https://x.com/fran6brg/photo" className="rounded-md" />
