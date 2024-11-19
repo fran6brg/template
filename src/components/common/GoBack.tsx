9c568c92f3c5de7f07b4983836f03124225f69af
Francis Berger <frcs.berger@gmail.com>
2024-11-20 00:07:01 +0100

    feat: setup

--- src/components/common/GoBack.tsx
+++ src/components/common/GoBack.tsx
@@ -7,7 +7,7 @@ import { IconIds } from '@/enums'
 export default function GoBack() {
     const router = useRouter()
     return (
-        <button onClick={() => router.back()} className="flex items-center gap-1 hover:text-secondary hover:underline">
+        <button onClick={() => router.back()} className="flex items-center gap-1 rounded-md p-1 hover:bg-very-light-hover hover:text-secondary">
             <IconWrapper icon={IconIds.CARBON_CHEVRON_LEFT} />
             <p className="text-base sm:text-sm">Back</p>
         </button>
