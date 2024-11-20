8d13e0130387f8bae981f4d3bd2cd60be8917c9d
Francis Berger <frcs.berger@gmail.com>
2024-11-20 13:33:33 +0100

    feat: setup

--- src/app/error.tsx
+++ src/app/error.tsx
@@ -4,18 +4,27 @@ import Button from '@/components/common/Button'
 import PageWrapper from '@/components/common/PageWrapper'
 import JsonField from '@/components/common/JsonField'
 import { useEffect } from 'react'
+import { extractErrorMessage } from '@/utils'
+import LinkWithIcon from '@/components/common/LinkWithIcon'
+import { APP_METADATA } from '@/config/app.config'
 
 export default function Error({ error, reset }: { error: Error & { digest?: string }; reset: () => void }) {
     useEffect(() => console.error(error), [error])
     return (
         <PageWrapper>
-            <div className="m-auto flex flex-col items-center gap-10">
-                <h2>Something went wrong!</h2>
-                <div className="flex flex-col items-center gap-1">
-                    <p>Error</p>
-                    <JsonField>{JSON.stringify(error, null, 2)}</JsonField>
+            <div className="my-10 flex flex-col items-center gap-6">
+                <p className="font-bold">Something went wrong!</p>
+                <div className="flex flex-col items-center gap-2 rounded-md border border-light-hover p-4">
+                    <p className="text-inactive">Application logs</p>
+                    <JsonField className="text-red-400">{JSON.stringify(extractErrorMessage(error), null, 2)}</JsonField>
                 </div>
-                <Button onClickFn={() => reset()} text="Reload page" />
+                ⬇️
+                <div className="flex flex-col items-center gap-3">
+                    <Button onClickFn={() => reset()} text="Reload page" />
+                    <p className="text-inactive">or</p>
+                    <LinkWithIcon href={`https://t.me/${APP_METADATA.SOCIALS.TELEGRAM}`}>@{APP_METADATA.SOCIALS.TELEGRAM}</LinkWithIcon>
+                    <p className="text-inactive">contact me on telegram</p>
+                </div>
             </div>
         </PageWrapper>
     )
--- src/components/common/JsonField.tsx
+++ src/components/common/JsonField.tsx
@@ -3,5 +3,5 @@
 import { cn } from '@/utils'
 
 export default function JsonField({ children, className }: { children?: React.ReactNode; className?: string }) {
-    return <pre className={cn('max-h-96 overflow-y-auto rounded-md bg-gray-200 p-2 text-xs text-gray-600', className)}>{children}</pre>
+    return <pre className={cn('max-h-96 overflow-y-auto rounded-md p-2 text-xs text-center text-wrap', className)}>{children}</pre>
 }
